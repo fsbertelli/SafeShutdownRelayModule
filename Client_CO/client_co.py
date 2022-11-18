cbcfa7a1cad9fda36ce90a9e020d083e016790d5
Felipe Bertelli dos Santos <42683582+fsbertelli@users.noreply.github.com>
2022-11-18 13:52:34 -0300

    Add files via upload

---
+++ Client_CO/client_co.py
@@ -0,0 +1,36 @@
+import socket
+import subprocess
+import time
+"""
+Esse é o que recebe o comando 
+"""
+
+HOST = '172.16.0.1'  
+PORT = 9009
+           # Porta que o Servidor esta
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+orig = (HOST, PORT)
+udp.bind(orig)
+
+def desligarasp():
+   # comando = "/usr/bin/sudo /sbin/shutdown -h now"
+    comando = "/usr/bin/sudo /sbin/shutdown -h now"
+    print(comando)
+    time.sleep(3)
+    import subprocess
+    processo = subprocess.Popen(comando.split(), stdout=subprocess.PIPE)
+    output = processo.communicate()[0]
+    print(output)
+      
+while True:
+    try:
+        msg, cliente = udp.recvfrom(1024)
+        if msg.decode() == 'batata':
+            desligarasp()
+        else:
+            print(cliente, msg.decode())
+    except Exception as e:
+        print(e)
+        udp.close()
+        break
+udp.close()
---
+++ Server_Stream/server.py
@@ -0,0 +1,67 @@
+#################################################
+#	Author: Felipe Bertelli dos Santos      	#
+#	Solinftec - Solutions Development 			#
+#	    Version 1.0 - 20/10/2022				#
+#################################################
+
+import RPi.GPIO as GPIO
+from time import sleep
+import socket
+import json
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Button to GPIO18
+
+
+def desligaMaster():
+    
+    with open("/home/solarbot/devices.json") as jf:
+        config = json.load(jf)
+
+    for inv in config:
+        hostname = inv["hostname"]
+        host = inv["host"]
+        port = inv["port"]
+
+        try:
+            print("[!] Conectado: " + inv["hostname"] + " " + inv["host"])
+            udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            dest = (inv["host"], inv["port"])
+
+            msg = ('batata')
+            while msg != '\x18':
+                print("[!] Enviando comando...") 
+                udp.sendto (msg.encode(), dest) 
+                udp.close()  
+                sleep(2)
+                       
+        except socket.error as e:
+                print ("[!] Erro ao conectar: %s" % e) 
+                                
+        finally:
+                udp.close()
+
+def desligaRasp():
+    import subprocess
+    comando = "/usr/bin/sudo shutdown -h now "
+    processo = subprocess.Popen(comando.split(), stdout=subprocess.PIPE)
+    sleep(2)
+    output = processo.communicate()[0]
+    print(output)
+
+try:
+
+    while True:
+        button_state = GPIO.input(18)
+        
+        if button_state == True:
+            print('[!] Desligando devices...')
+            desligaMaster()
+            sleep(0.5)
+            GPIO.cleanup()
+            sleep(1)
+            print('[!] Desligando PU')
+            desligaRasp()    
+except:
+    GPIO.cleanup()
